8b31b8654441a038a7db564daf8c0a52b51d9a00
Mihail-D <nuby.4register@gmail.com>
2019-07-24 21:13:55 +0300

    +

--- js/setup.js
+++ js/setup.js
@@ -1,12 +1,5 @@
 'use strict';
 
-var openSetupMenuButton = document.querySelector('.setup-open');
-var closeSetupMenuButton = document.querySelector('.setup-close');
-var discloseSetup = document.querySelector('.setup');
-var discloseSetupSimilar = document.querySelector('.setup-similar');
-var userNameField = document.querySelector('.setup-user-name');
-var formMain = document.querySelector('.setup');
-
 var similarCharacters = [];
 var WIZARD_NAMES = ['Иван', 'Хуан', 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
@@ -20,41 +13,6 @@ var COATS_COLOR = [
 ];
 var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 var FIREBALLS_COLOR = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
-
-var setupWindowCoords = {
-  x: formMain.style.left,
-  y: formMain.style.top,
-};
-
-var menuToggleView = function () {
-  formMain.style.top = setupWindowCoords.y;
-  formMain.style.left = setupWindowCoords.x;
-  discloseSetup.classList.toggle('hidden');
-  discloseSetupSimilar.classList.toggle('hidden');
-};
-
-document.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === 27 && userNameField !== document.activeElement) {
-    formMain.style.top = setupWindowCoords.y;
-    formMain.style.left = setupWindowCoords.x;
-    discloseSetup.classList.add('hidden');
-    discloseSetupSimilar.classList.add('hidden');
-  }
-});
-
-openSetupMenuButton.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === 13) {
-    menuToggleView();
-  }
-});
-
-openSetupMenuButton.addEventListener('click', function () {
-  menuToggleView();
-});
-
-closeSetupMenuButton.addEventListener('click', function () {
-  menuToggleView();
-});
 
 // генератор случайного числа
 var getRandomNumber = function (minValue, maxValue) {
@@ -124,56 +82,3 @@ for (var j = 0; j < 4; j++) {
 }
 
 similarListElement.appendChild(wizardFragment);
-
-// реализация drag'n'drop
-
-(function () {
-  var dialogHandler = formMain.querySelector('.upload');
-
-  dialogHandler.addEventListener('mousedown', function (evt) {
-    evt.preventDefault();
-
-    var startCoords = {
-      x: evt.clientX,
-      y: evt.clientY,
-    };
-
-    var dragged = false;
-
-    var onMouseMove = function (moveEvt) {
-      moveEvt.preventDefault();
-      dragged = true;
-
-      var shift = {
-        x: startCoords.x - moveEvt.clientX,
-        y: startCoords.y - moveEvt.clientY,
-      };
-
-      startCoords = {
-        x: moveEvt.clientX,
-        y: moveEvt.clientY,
-      };
-
-      formMain.style.top = formMain.offsetTop - shift.y + 'px';
-      formMain.style.left = formMain.offsetLeft - shift.x + 'px';
-    };
-
-    var onMouseUp = function (upEvt) {
-      upEvt.preventDefault();
-
-      document.removeEventListener('mousemove', onMouseMove);
-      document.removeEventListener('mouseup', onMouseUp);
-
-      if (dragged) {
-        var onClickPreventDefault = function (dragEvt) {
-          dragEvt.preventDefault();
-          dialogHandler.removeEventListener('click', onClickPreventDefault);
-        };
-        dialogHandler.addEventListener('click', onClickPreventDefault);
-      }
-    };
-
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseup', onMouseUp);
-  });
-})();
